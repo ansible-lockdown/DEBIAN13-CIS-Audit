8a662e0fa3c0b8d4e24b11d372c4de2449fa1d66
Mark Bolwell <mark.bollyuk@gmail.com>
2026-09-15 10:57:12 +0100

    Updated version
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- run_audit.sh
+++ run_audit.sh
@@ -28,7 +28,7 @@
 
 # Goss benchmark variables (these should not need changing unless new release)
 BENCHMARK=CIS # Benchmark Name aligns to the audit
-BENCHMARK_VER=1.0.0
+BENCHMARK_VER=1.1.0
 BENCHMARK_OS=DEBIAN13
 
 # Goss host Variables
